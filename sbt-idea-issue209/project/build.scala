07972ca8d25f027f5af2af504fc000eef0de3f7c
Thomas Dufour <td@thomasdufour.fr>
2013-10-16 19:43:06 +0200

    Add sbt-protobut to testcase for gen-idea

--- sbt-idea-issue209/project/build.scala
+++ sbt-idea-issue209/project/build.scala
@@ -1,8 +1,11 @@
 import sbt._
+import Keys._
 import com.github.retronym.sbtxjc.SbtXjcPlugin
+import sbtprotobuf.{ProtobufPlugin => PB}
 
 object build extends Build {
-  lazy val root = Project("main", file("."), settings = Defaults.defaultSettings ++ SbtXjcPlugin.xjcSettings ++ Seq(
+  lazy val root = Project("main", file("."), settings = Defaults.defaultSettings ++ SbtXjcPlugin.xjcSettings ++ PB.protobufSettings ++ Seq(
+    version in PB.protobufConfig := "2.5.0",
     SbtXjcPlugin.xjcCommandLine += "-verbose"
   ))
 }
--- sbt-idea-issue209/project/plugins.sbt
+++ sbt-idea-issue209/project/plugins.sbt
@@ -1,3 +1,5 @@
 addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.2.0")
 
-addSbtPlugin("com.github.retronym" % "sbt-xjc" % "0.5")
+addSbtPlugin("org.scala-sbt.plugins" % "sbt-xjc" % "0.5")
+
+addSbtPlugin("com.github.gseitz" % "sbt-protobuf" % "0.3.0")
